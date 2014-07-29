b259e5b32be8362383695464f871966ca5d051b7
p0lax <polax@list.ru>
2014-07-29 22:52:39 +0400

    Change get audio function

--- main.js
+++ main.js
@@ -1,34 +1,30 @@
 function getAudio() {
 
-    var musicEls = document.getElementsByClassName("audio");
+    var musicEls = document.querySelectorAll("#content .audio:not([passed])"),
+        musicElsLength = musicEls.length;
 
-    for (var i = 0; i < musicEls.length; i++) {
-        if (musicEls[i].firstChild.href) {
-            musicEls[i].firstChild.style.top = musicEls[i].getElementsByClassName('play_new')[0].offsetTop + "px";
-            continue;
-        }
+    for (var i = 0; i < musicElsLength; i += 1) {
 
-        var _mp3_ = musicEls[i].getElementsByTagName("input")[0].value.split("?")[0]; // get audio url
-        var _b_ = main_div[i].getElementsByTagName("b")[0];
-        var _top_ = document.getElementById(main_div[0].parentNode.id.replace('audio', 'play')).offsetTop + "px";
-        var _t_ = main_div[i].getElementsByClassName('play_new')[0].offsetTop + "px";
-        var span = main_div[i].getElementsByTagName('span')[0];
-        var ispan = span.innerHTML;
+        var _mp3_ = musicEls[i].getElementsByTagName("input")[0].value.split("?")[0], // get audio url
+            _b_ = musicEls[i].getElementsByTagName("b")[0],
+            _t_ = musicEls[i].getElementsByClassName('play_new')[0].offsetTop + "px",
+            span = musicEls[i].getElementsByTagName('span')[0],
+            ispan = span.innerHTML,
+            _a_ = document.createElement("a");
+
         if (span.children[0]) {
-            var ispan = span.children[0].innerHTML;
+            ispan = span.children[0].innerHTML;
         }
-        var _a_ = document.createElement("a");
-        _a_.href = _mp3_.substring(0, _mp3_.indexOf('.mp3') + 4);
+
+        _a_.href = _mp3_;
         _a_.download = trim(_b_.children[0].innerHTML + "-" + ispan) + ".mp3";
-        _a_.title = 'Скачать ' + _b_.children[0].innerHTML + "-" + ispan;
+        _a_.title = 'Download ' + _b_.children[0].innerHTML + "-" + ispan;
         _a_.draggable = "true";
-        _a_.innerHTML = '<span style="cursor:copy;padding:6px;width:16px;height:16px;background-color:#5F7D9D;color:#fff;border-radius:2px;"><img src="http://st0.vk.me/images/icons/darr.gif" /></span>';
-        _a_.style.zIndex = "10";
-        _a_.style.left = "30px";
-        _a_.style.top = _t_;
-        _a_.style.position = "absolute";
-        main_div[i].parentNode.insertBefore(_a_, main_div[i].parentNode.firstChild);
+        _a_.innerHTML = '<span style="cursor:pointer; padding:6px; width:16px; height:16px; background-color:#5F7D9D; color:#fff; border-radius: 10px;"><img src="http://st0.vk.me/images/icons/darr.gif" /></span>';
+        _a_.setAttribute('style', "position: absolute; left: 30px; z-index: 10;" + "top: " + _t_);
+        musicEls[i].insertBefore(_a_, musicEls[i].firstChild);
         _b_.style.paddingLeft = "27px";
+        musicEls[i].setAttribute('passed', "");
     }
 }
 
@@ -177,7 +173,7 @@ setInterval(function () {
             videovk();
         }
         if (document.getElementsByClassName("area clear_fix").length != 0) {
-            audio();
+            getAudio();
         }
     }
 }, 1000);
